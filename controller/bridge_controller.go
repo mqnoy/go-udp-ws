5c067582099d2f83b0cca678f625c951f1b8ea08
mqnoy <qnoy.rifky@gmail.com>
2022-06-02 00:19:39 +0700

    edit: sperated

--- controller/bridge_controller.go
+++ controller/bridge_controller.go
@@ -3,8 +3,7 @@ package controller
 import (
 	"bebuhcon/utils"
 	"log"
-
-	"github.com/gorilla/websocket"
+	"net"
 )
 
 // us "bebuhcon/services/udp_services"
@@ -24,28 +23,31 @@ func (uss *BridgeController) ParseDatagramUdp(addr string, datagram string) {
 
 	log.Println("BridgeController::ReadRequest - Received ", datagram, " from ", addr)
 
-	// var np *net.PacketConn = utils.UdpMapped["default1"]
-	// var pc net.PacketConn = *np
-
-	var wn *websocket.Conn = utils.WsMapped["default1"]
-	var wc websocket.Conn = *wn
+	wn := utils.WsMapped["default1"]
 
 	p := []byte("command")
-	// ipaddr, err := net.ResolveIPAddr("ip", addr)
-	// if err != nil {
-	// handle error
-	// } else {
-	// pc.WriteTo(p, ipaddr)
 
-	if err := wc.WriteMessage(1, p); err != nil {
+	if err := wn.WriteMessage(1, p); err != nil {
 		log.Println(err)
 	}
-
-	// }
 
 }
 
 // func parsing request websocket
 func (uss *BridgeController) ParseRequestWebsocket(addr string, requests string) {
 	log.Println("BridgeController::ParseRequestWebsocket - Received ", requests, " from ", addr)
+
+	var np *net.PacketConn = utils.UdpMapped["default1"]
+	var pc net.PacketConn = *np
+
+	p := []byte("command")
+	ipaddr, err := net.ResolveIPAddr("ip", addr)
+	if err != nil {
+		// handle error
+	} else {
+
+		if _, errWu := pc.WriteTo(p, ipaddr); errWu != nil {
+			log.Println(errWu)
+		}
+	}
 }
